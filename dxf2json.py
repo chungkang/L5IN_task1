676bae31665469338b466cb06be2071d6f05f216
unknown <chungkang.c@gmail.com>
2021-12-20 16:15:57 +0100

    ''

--- dxf2json.py
+++ dxf2json.py
@@ -1,4 +1,12 @@
 import ezdxf
 dxf = "./HCU_D_104_Grundriss_2OG_moved.dxf"
+
+# loading dxf file
 doc = ezdxf.readfile(dxf)
-print(doc)
+
+# get modelspace
+msp = doc.modelspace()
+
+# get lines of layer
+lines = msp.query('LINE[layer=="AUSBAU - Darstellungen - Daemmung"]')
+
