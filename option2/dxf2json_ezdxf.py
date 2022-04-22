84454365b6f086ab65f8dfcce83efdb1daea4f6c
unknown <chungkang.c@gmail.com>
2022-04-22 12:55:07 +0200

    add door start points

--- option2/dxf2json_ezdxf.py
+++ option2/dxf2json_ezdxf.py
@@ -3,7 +3,6 @@ import ezdxf.addons.geo as geo
 import json
 import geopandas
 from shapely.ops import polygonize
-import shapely
 
 # source file
 dxf_name = "layer_HCU_D_106_Grundriss_4OG_moved_V2"
@@ -13,23 +12,6 @@ doc = ezdxf.readfile("dxf\\"+ dxf_name + ".dxf")
 
 # get modelspace / 모형
 msp = doc.modelspace()
-
-# get layout / plan - layout page
-# plan = doc.layout('16 - plan 2.OG_1_100')
-
-# myText = open(r'my_text_file.txt','w')
-# myText.write(str(msp.query('INSERT')))
-# myText.close()
-
-# Get the geo location information from the DXF file:
-# geo_data = msp.get_geodata()
-
-# if geo_data:
-#     # Get transformation matrix and epsg code:
-#     m, epsg = geo_data.get_crs_transformation()
-# else:
-#     # Identity matrix for DXF files without geo reference data:
-#     m = ezdxf.math.Matrix44()
 
 # initialize empty geojson
 geojson_format = {
@@ -71,6 +53,57 @@ layer_list = [
 # counting entities of dxf file => index 
 idx = 0
 
+
+# door block start point
+point_idx = 0
+
+geojson_door_format = {
+    "type": "FeatureCollection",
+	"crs": {
+	    "type": "name",
+		"properties": {
+			"name": "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+		}
+	},
+    "features": []
+}
+
+for insert in msp.query("INSERT[layer=='AUSBAU - Objekte - Tueren']"):
+    block = insert.dxf.insert
+    angle = block.angle
+    angle_deg = block.angle_deg
+    magnitude = block.magnitude
+    magnitude_square = block.magnitude_square
+    magnitude_xy = block.magnitude_xy
+    x = block.x
+    y = block.y
+
+    each_feature = {
+        "type": "Feature",
+        "properties": {
+            "index": point_idx,
+            "layer": 'AUSBAU - Objekte - Tueren',
+            "category": 'door_point'
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [x, y]
+        }
+    }
+
+    geojson_door_format["features"].append(each_feature)
+    point_idx += 1
+
+# write custom defined CRS geojson
+with open( 'option2\\option2_door_point.geojson', 'wt', encoding='utf8') as fp:
+    json.dump(geojson_door_format, fp, indent=2)
+
+# read created geojson / reprojection to EPSG:32632 / write reprojected geojson
+loaded_geojson = geopandas.read_file('option2\\option2_door_point.geojson')
+loaded_geojson = loaded_geojson.to_crs("EPSG:32632")
+loaded_geojson.to_file("option2\\option2_door_pointEPSG32632.geojson", driver='GeoJSON')
+
+# explode all the blocks
 for flag_ref in msp.query("INSERT"):
     flag_ref.explode()
 
@@ -114,7 +147,6 @@ with open( 'option2\\option2.geojson', 'wt', encoding='utf8') as fp:
 loaded_geojson = geopandas.read_file('option2\\option2.geojson')
 loaded_geojson = loaded_geojson.to_crs("EPSG:32632")
 loaded_geojson.to_file("option2\\option2_EPSG32632.geojson", driver='GeoJSON')
-
 
 # lineString to polygon [door]
 # linestrings = gdf[gdf.geometry.type == "LineString"]
